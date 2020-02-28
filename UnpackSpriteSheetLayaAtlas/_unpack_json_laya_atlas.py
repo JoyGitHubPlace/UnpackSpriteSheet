5b7a09903745918dd48f04be092c25e62d7b63ed
JoyGitHubPlace <1175725736@qq.com>
2020-02-28 10:46:01 +0800

    Update _unpack_json_laya_atlas.py

--- UnpackSpriteSheetLayaAtlas/_unpack_json_laya_atlas.py
+++ UnpackSpriteSheetLayaAtlas/_unpack_json_laya_atlas.py
@@ -3,6 +3,7 @@
 import os,sys
 import json
 import os
+import traceback
 import os.path
 from PIL import Image
 
@@ -13,7 +14,19 @@ def json_to_dict(json_filename):
     for one_pic_item in all_pic_dic['frames']:
         one_json_item = all_pic_dic['frames'][one_pic_item]
         one_item = {}
-        one_item['name'] = one_pic_item.strip().lstrip().rstrip(',')
+        one_name = one_pic_item.strip().lstrip().rstrip(',')
+        if one_name.find('/')>=0:
+            o_dir =''
+            d_list =one_name.split('/')
+            for each in d_list:
+                if each.find('.')>=0:
+                    one_item['name'] = each
+                else:
+                    o_dir =o_dir+'/'+each
+            one_item['dir'] = o_dir
+        else:
+            one_item['name'] = one_pic_item.strip().lstrip().rstrip(',')
+            one_item['dir'] = ''
         one_item['x'] = one_json_item['frame']['x']
         one_item['y'] = one_json_item['frame']['y']
         one_item['w'] = one_json_item['frame']['w']
@@ -37,6 +50,7 @@ def gen_png_from_json(folder_name, json_filename, png_filename):
 
     for i, one_item_data in enumerate(all_item_list):
         file_name = one_item_data['name']
+        dir_name = one_item_data['dir']
         x = one_item_data['x']
         y = one_item_data['y']
         w = one_item_data['w']
@@ -45,8 +59,22 @@ def gen_png_from_json(folder_name, json_filename, png_filename):
         #设置图像裁剪区域 (x左上，y左上，x右下,y右下)
         image_box = [x, y, x + w , y + h ]
         one_pic = big_image.crop(image_box)
+        if one_item_data['dir'] == '':
+            one_pic.save(folder_name + "/" + file_name) # 存储裁剪得到的图像
+        else:
+            d_list = dir_name.split('/')
+            o_dir = '/'
+            for each in d_list:
+                if each=='':
+                    o_dir =o_dir+''
+                else:
+                    o_dir =o_dir+'/'+ each
+                    p = folder_name + o_dir
+                    if not os.path.isdir(p):
+                        os.mkdir(p)
 
-        one_pic.save(folder_name + "/" + file_name) # 存储裁剪得到的图像
+            one_pic.save(p + "/" + file_name) # 存储裁剪得到的图像
+        
         
         #print one_item_data
 
@@ -75,6 +103,7 @@ if __name__ == '__main__':
                     gen_png_from_json(os.path.join(rootdir, file_name), json_filename, png_filename )
                 except Exception:
                     print '!!!!!!!!!!!!!!!!!!!!' + json_filename + ' json error !!!!!!!!!!!!!!!!!!!!!'
+                    print(traceback.format_exc())
             elif os.path.exists(jpg_filename):
                 try:
                     gen_png_from_json(os.path.join(rootdir, file_name), json_filename, jpg_filename )
